e178da67f0b8ddffa5d09725c2fe15a977990734
Erfan <143827987+erfjab@users.noreply.github.com>
2025-08-04 23:51:08 +0330

    feat[db]: config alembic and base (#3)
    * feat[db]: config alembic and base
    
    * fix[format]
    
    * chore[db]

--- src/config/env.py
+++ src/config/env.py
@@ -5,3 +5,11 @@ load_dotenv(override=True)
 
 ### Develop Settings
 DEBUG = config("DEBUG", default=False, cast=bool)
+
+### Database Settings
+DATABASE_USERNAME = config("DATABASE_USERNAME", default="", cast=str)
+DATABASE_PASSWORD = config("DATABASE_PASSWORD", default="", cast=str)
+DATABASE_HOST = config("DATABASE_HOST", default="localhost", cast=str)
+DATABASE_PORT = config("DATABASE_PORT", default=5432, cast=int)
+DATABASE_NAME = config("DATABASE_NAME", default="", cast=str)
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
