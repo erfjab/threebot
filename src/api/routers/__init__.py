ac9293685a77fab7eddbfa3864d886b868e49ae6
erfjab <erfjab@gmail.com>
2025-08-05 00:31:18 +0330

    feat[bot]: db models, handlers, lang, statemanager

--- src/api/routers/__init__.py
+++ src/api/routers/__init__.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
-from . import base
+from . import base, telegram
 
 
 def setup_api_routers() -> APIRouter:
     router = APIRouter()
     router.include_router(base.router)
+    router.include_router(telegram.router)
     return router
 
 
